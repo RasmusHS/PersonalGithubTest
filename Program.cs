cfb3f42468d222a4f8ac05118cea2ae6b026f955
RasmusHS <rhsc38205@edu.ucl.dk>
2021-11-11 09:37:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalGithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
